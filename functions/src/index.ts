2c84dd367d934e0151f6b348b6ce3f479d0542dc
Khang Tran <71557127+khangtrannn@users.noreply.github.com>
2024-10-28 06:09:24 +0000

    feat(): add authentication

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,17 @@
+import validateFirebaseIdToken from "./authMiddleware";
+
 const { onRequest } = require("firebase-functions/v2/https");
 const { initializeApp } = require("firebase-admin/app");
 const cors = require('cors')
+const express = require('express');
 
 const postRoutes = require('./postRoutes');
 
 initializeApp();
 
-const express = require('express');
 const app = express();
+
+app.use(validateFirebaseIdToken);
 
 app.use(express.json());
 app.use(cors());
